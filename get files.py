8880c3b283d3d7008c2a558435babd4ee97820b9
Mitch Tassicker <mitchtassicker@gmail.com>
2023-06-04 12:18:52 +0800

    fixed buttons not showing full text

--- get files.py
+++
@@ -1,8 +0,0 @@
-import os, sys
-
-path = "D:/Project/FlagMaster/flags"
-filelist = [file for file in os.listdir(path) if file.endswith(".png")]
-print(filelist)
-
-flag_dictionary = dict.fromkeys(filelist, "Empty")
-print(flag_dictionary)
